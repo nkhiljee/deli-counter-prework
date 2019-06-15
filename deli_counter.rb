06a5d59adb2581eee6d0d6000c6268e193b1bd3e
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 07:24:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+katzdeli = []
+def line(katzdeli)
+  if katzdeli.length > 0 
+    puts "Your current place in line is #{katzdeli.length}."
+  else
+    puts "The line is curently empty."
+  end
+end
